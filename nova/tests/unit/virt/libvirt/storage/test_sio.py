328795655e91aca2ecf173027b5ca8048a302ff0
Andrey Pavlov <andrey.mp@gmail.com>
2016-11-29 12:29:13 +0300

    add more simple tests for sio_utils

--- nova/tests/unit/virt/libvirt/storage/test_sio.py
+++ nova/tests/unit/virt/libvirt/storage/test_sio.py
@@ -19,15 +19,7 @@ from nova.virt.libvirt.storage import sio_utils
 CONF = cfg.CONF
 
 
-class VolumeNotFound(BaseException):
-    pass
-
-
-class VolumeNotMapped(BaseException):
-    pass
-
-
-class VolumeAlreadyMapped(BaseException):
+class FakeException(BaseException):
     pass
 
 
@@ -108,11 +100,11 @@ class SioTestCase(test.NoDBTestCase):
 
     @mock.patch.object(sio_utils, 'siolib')
     def test_remove_volume_with_error(self, mock_siolib):
-        self.ioctx.delete_volume.side_effect = BaseException
-        self.assertRaises(BaseException, self.driver.remove_volume,
+        self.ioctx.delete_volume.side_effect = FakeException
+        self.assertRaises(FakeException, self.driver.remove_volume,
                           self.vol_id)
 
-        mock_siolib.VolumeNotFound = VolumeNotFound
+        mock_siolib.VolumeNotFound = FakeException
         self.ioctx.delete_volume.side_effect = mock_siolib.VolumeNotFound
         self.driver.remove_volume(self.vol_id)
         self.ioctx.delete_volume.assert_called_with(
@@ -133,10 +125,10 @@ class SioTestCase(test.NoDBTestCase):
     def test_map_volume_with_error(self, mock_siolib):
         self.ioctx.get_volumepath.return_value = '/a/b/c'
 
-        self.ioctx.attach_volume.side_effect = BaseException
-        self.assertRaises(BaseException, self.driver.map_volume, self.vol_id)
+        self.ioctx.attach_volume.side_effect = FakeException
+        self.assertRaises(FakeException, self.driver.map_volume, self.vol_id)
 
-        mock_siolib.VolumeAlreadyMapped = VolumeAlreadyMapped
+        mock_siolib.VolumeAlreadyMapped = FakeException
         self.ioctx.attach_volume.side_effect = mock_siolib.VolumeAlreadyMapped
         self.driver.map_volume(self.vol_id)
         self.ioctx.attach_volume.assert_called_with(
@@ -152,16 +144,16 @@ class SioTestCase(test.NoDBTestCase):
 
     @mock.patch.object(sio_utils, 'siolib')
     def test_unmap_volume_with_error(self, mock_siolib):
-        self.ioctx.detach_volume.side_effect = BaseException
-        self.assertRaises(BaseException, self.driver.unmap_volume, self.vol_id)
+        self.ioctx.detach_volume.side_effect = FakeException
+        self.assertRaises(FakeException, self.driver.unmap_volume, self.vol_id)
 
-        mock_siolib.VolumeNotMapped = VolumeNotMapped
+        mock_siolib.VolumeNotMapped = FakeException
         self.ioctx.detach_volume.side_effect = mock_siolib.VolumeNotMapped
         self.driver.unmap_volume(self.vol_id)
         self.ioctx.detach_volume.assert_called_with(
             self.vol_id, self.sdc_uuid)
 
-        mock_siolib.VolumeNotFound = VolumeNotFound
+        mock_siolib.VolumeNotFound = FakeException
         self.ioctx.detach_volume.side_effect = mock_siolib.VolumeNotFound
         self.driver.unmap_volume(self.vol_id)
         self.ioctx.detach_volume.assert_called_with(
@@ -175,7 +167,7 @@ class SioTestCase(test.NoDBTestCase):
 
     @mock.patch.object(sio_utils, 'siolib')
     def test_get_volume_id_with_error(self, mock_siolib):
-        mock_siolib.VolumeNotFound = VolumeNotFound
+        mock_siolib.VolumeNotFound = FakeException
         self.ioctx.get_volumeid.side_effect = mock_siolib.VolumeNotFound
         self.assertRaises(mock_siolib.VolumeNotFound,
                           self.driver.get_volume_id, self.vol_name)
@@ -184,8 +176,8 @@ class SioTestCase(test.NoDBTestCase):
         self.assertIsNone(result)
         self.ioctx.get_volumeid.assert_called_with(self.vol_name)
 
-        self.ioctx.get_volumeid.side_effect = BaseException
-        self.assertRaises(BaseException, self.driver.get_volume_id,
+        self.ioctx.get_volumeid.side_effect = FakeException
+        self.assertRaises(FakeException, self.driver.get_volume_id,
                           self.vol_name)
 
     @mock.patch.object(sio_utils, 'siolib')
@@ -195,9 +187,34 @@ class SioTestCase(test.NoDBTestCase):
         self.assertTrue(result)
         self.ioctx.get_volumeid.assert_called_with(self.vol_name)
 
-        mock_siolib.VolumeNotFound = VolumeNotFound
+        mock_siolib.VolumeNotFound = FakeException
         self.ioctx.get_volumeid.side_effect = mock_siolib.VolumeNotFound
         result = self.driver.get_volume_id(self.vol_name,
                                            none_if_not_found=True)
         self.assertFalse(result)
         self.ioctx.get_volumeid.assert_called_with(self.vol_name)
+
+    def test_get_volume_name(self):
+        self.ioctx.get_volumename.return_value = self.vol_id
+        result = self.driver.get_volume_name(self.vol_name)
+        self.assertEqual(self.vol_id, result)
+        self.ioctx.get_volumename.assert_called_with(self.vol_name)
+
+    @mock.patch.object(sio_utils, 'siolib')
+    def test_get_volume_path(self, mock_siolib):
+        self.ioctx.get_volumepath.return_value = '/a/b'
+        result = self.driver.get_volume_path(self.vol_id)
+        self.assertEqual('/a/b', result)
+        self.ioctx.get_volumepath.assert_called_with(self.vol_id)
+
+        mock_siolib.VolumeNotMapped = FakeException
+        self.ioctx.get_volumepath.side_effect = mock_siolib.VolumeNotMapped
+        result = self.driver.get_volume_path(self.vol_id)
+        self.assertIsNone(result)
+        self.ioctx.get_volumepath.assert_called_with(self.vol_id)
+
+    def test_get_volume_size(self):
+        self.ioctx.get_volumesize.return_value = 2
+        result = self.driver.get_volume_size(self.vol_id)
+        self.assertEqual(2 * 1024, result)
+        self.ioctx.get_volumesize.assert_called_with(self.vol_id)
